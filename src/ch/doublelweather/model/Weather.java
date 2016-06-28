01ae1f18b53e32a43c7c6e0b9cdd777206a9f98a
zmartl-bbc <luca.marti@bluewin.ch>
2016-06-28 13:44:13 +0200

    controller and model
    Signed-off-by: zmartl-bbc <luca.marti@bluewin.ch>

---
+++ src/ch/doublelweather/model/Weather.java
@@ -0,0 +1,14 @@
+/**
+ * 
+ */
+package ch.doublelweather.model;
+
+/**
+ * @description
+ * @author luca.marti
+ * @year 2016
+ * @version 1.0
+ */
+public class Weather {
+	private String weatherDescription;
+}
