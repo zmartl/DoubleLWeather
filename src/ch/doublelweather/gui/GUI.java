852ffcbf7914d1c24b6c56fe22f96ca9ab46183a
LucaZatti <luca.zatti@bluewin.ch>
2016-06-28 13:57:23 +0200

    add class gui

---
+++ src/ch/doublelweather/gui/GUI.java
@@ -0,0 +1,27 @@
+package ch.doublelweather.gui;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JButton;
+import javax.swing.SwingUtilities;
+import javax.swing.JPanel;
+import java.awt.BorderLayout;
+
+public class GUI extends JFrame implements ActionListener
+{
+    private static final long serialVersionUID = 1L;
+
+    public GUI()
+    {
+        super("DoubleLWeather");
+
+    }
+
+    public static void main(String[] args)
+    {
+        
+    }
+}
+
